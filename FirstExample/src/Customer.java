967bd661b7d3428c305c0b8ea2d91aac9b0b75f7
delgemoon <delgemoon@vietnamz.org>
2019-05-29 22:15:16 +0700

    renameing the extract method

--- FirstExample/src/Customer.java
+++ FirstExample/src/Customer.java
@@ -40,24 +40,24 @@ public class Customer {
         return result;
     }
 
-    private double amoutFor(Rental each) {
-        double thisAmount = 0;
-        switch(each.getMovie().getPriceCode()) {
+    private double amoutFor(Rental rental) {
+        double result = 0;
+        switch(rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                result += 2;
+                if (rental.getDaysRented() > 2) {
+                    result += (rental.getDaysRented() - 2) * 1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                result += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                result += 1.5;
+                if (rental.getDaysRented() > 3) {
+                    result += (rental.getDaysRented() - 3) * 1.5;
                 }
         }
-        return thisAmount;
+        return result;
     }
 }
