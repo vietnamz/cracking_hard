980321e373d8ba741694e3d6e6c0f2f7688b4766
delgemoon <delgemoon@vietnamz.org>
2019-05-29 22:22:13 +0700

    moving amoutFor to rental class

--- FirstExample/src/Customer.java
+++ FirstExample/src/Customer.java
@@ -41,23 +41,6 @@ public class Customer {
     }
 
     private double amoutFor(Rental rental) {
-        double result = 0;
-        switch(rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (rental.getDaysRented() > 2) {
-                    result += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (rental.getDaysRented() > 3) {
-                    result += (rental.getDaysRented() - 3) * 1.5;
-                }
-        }
-        return result;
+        rental.getCharge();
     }
 }
